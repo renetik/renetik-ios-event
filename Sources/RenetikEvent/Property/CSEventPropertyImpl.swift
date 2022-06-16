b8f9e47aaf025a0bf7ff00f5dd20369db27b1a82
Rene Dohan <dohan.rene@gmail.com>
2022-06-16 01:08:00 -0500

    Update CSEventPropertyImpl.swift

--- Sources/RenetikEvent/Property/CSEventPropertyImpl.swift
+++ Sources/RenetikEvent/Property/CSEventPropertyImpl.swift
@@ -1,46 +1,7 @@
-open class CSEventPropertyImpl<T>: CSEventPropertyProtocol where T: Equatable {
-
-    private let eventChange: CSEvent<T> = event()
-    private var onApply: ((T) -> ())?
-    private var _value: T
-
-    public var value: T {
-        get { _value }
-        set { value(newValue, fireEvents: true) }
-    }
-
-    public init(value: T, onApply: ((T) -> ())? = nil) {
-        _value = value
-        self.onApply = onApply
-    }
-
-    @discardableResult
-    public func value(_ newValue: T, fireEvents: Bool = true) -> Self {
-        if _value == newValue { return self }
-        _value = newValue
-        if (fireEvents) {
-            onApply?(newValue)
-            eventChange.fire(newValue)
-        }
-        return self
-    }
-
-    @discardableResult
-    public func onChange(_ function: @escaping ArgFunc<T>) -> CSRegistration {
-        eventChange.listen { function($0) }
-    }
-
-    @discardableResult
-    public func apply() -> Self {
-        onApply?(value)
-        eventChange.fire(value)
-        return self
-    }
-}
-
-// In android CSEventPropertyBase
 open class CSEventProperty<T>: CSObject, CSEventPropertyProtocol where T: Equatable {
-    private var onApply: ((T) -> ())?
+
+    var onApply: ((T) -> ())?
+    let eventChange: CSEvent<T> = event()
 
     public init(onApply: ((T) -> ())? = nil) {
         self.onApply = onApply
@@ -50,8 +11,6 @@ open class CSEventProperty<T>: CSObject, CSEventPropertyProtocol where T: Equata
         get { fatalError("value has not been implemented") }
         set { }
     }
-
-    private let eventChange: CSEvent<T> = event()
 
     @discardableResult
     public func onChange(_ function: @escaping ArgFunc<T>) -> CSRegistration {
@@ -66,5 +25,31 @@ open class CSEventProperty<T>: CSObject, CSEventPropertyProtocol where T: Equata
     open func onValueChanged(_ newValue: T, fire: Boolean = true) {
         onApply?(value)
         if fire { eventChange.fire(newValue) }
+    }
+}
+
+open class CSEventPropertyImpl<T>: CSEventProperty<T> where T: Equatable {
+
+    private var _value: T
+
+    public init(value: T, onApply: ((T) -> ())? = nil) {
+        _value = value
+        super.init(onApply: onApply)
+    }
+
+    public override var value: T {
+        get { _value }
+        set { value(newValue, fireEvents: true) }
+    }
+
+    @discardableResult
+    public func value(_ newValue: T, fireEvents: Bool = true) -> Self {
+        if _value == newValue { return self }
+        _value = newValue
+        if (fireEvents) {
+            onApply?(newValue)
+            eventChange.fire(newValue)
+        }
+        return self
     }
 }
